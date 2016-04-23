ca1f974adb61ec3db90f3750e0d67716e7e59fb7
Hoang Gia Minh <hgminh95@gmail.com>
2016-04-23 12:05:47 +0700

    Update test data

--- js/test.js
+++ js/test.js
@@ -5,30 +5,55 @@ var generateFakeData = function() {
         },
         matches: [
             {
+                player1: 'Player 1',
+                player2: 'Player 2',
+                score1: 3,
+                score2: 2,
                 group: 0,
-                position: 0,
+                position: 0
             },
             {
+                player1: 'Player 3',
+                player2: 'Player 4',
+                score1: 3,
+                score2: 0,
                 group: 0,
                 position: 40
             },
             {
+                player1: 'Very long player"s name',
+                player2: 'short',
+                score1: 1,
+                score2: 3,
                 group: 0,
                 position: 80
             },
             {
+                player1: 'Player 5',
+                player2: 'Player 6',
+                // Missing score means this match have not been played.
                 group: 0,
                 position: 120
             },
             {
+                player1: 'Player 1',
+                player2: 'Player 3',
+                link1: 0,
+                link2: 1,
                 group: 1,
                 position: 20
             },
             {
+                player1: 'short',
+                // Missing player's name means
+                link1: 2,
+                link2: 3,
                 group: 1,
                 position: 100
             },
             {
+                link1: 4,
+                link2: 5,
                 group: 2,
                 position: 60
             }
