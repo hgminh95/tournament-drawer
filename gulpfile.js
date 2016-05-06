fa92e22cd7cd5267a9e1bd5bc6c94a58a4eec925
Hoang Gia Minh <hgminh95@gmail.com>
2016-05-07 00:28:46 +0700

    Use babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
 var del = require('del');
 
 var paths = {
-    scripts: ['js/core.js', 'js/test.js']
+    scripts: ['js/core.js', 'js/elimination.js']
 }
 
 gulp.task('clean', function() {
@@ -14,6 +15,9 @@ gulp.task('clean', function() {
 
 gulp.task('scripts', ['clean'], function() {
     return gulp.src(paths.scripts)
+        .pipe(babel({
+			presets: ['es2015']
+		}))
         .pipe(sourcemaps.init())
             .pipe(uglify().on('error', function(e) {
                 console.log(e)
